01b42728f4f87187d848fdf8e11b095f1d99c1b3
Shigana <76201160+Shigana@users.noreply.github.com>
2020-12-19 20:11:46 +0900

    Add files via upload

---
+++ Count.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Count : MonoBehaviour
+{
+    public countCSV rWallF;
+    public countCSV rWallB;
+    public countCSV rWallR;
+    public countCSV rWallL;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    void OnTriggerEnter(Collider other) //OnCollisionEnter is called when objects collide with each other
+    {
+        if (other.gameObject.CompareTag("WallF"))
+        {
+            rWallF.GetComponent<countCSV>().wallF++;
+        }
+        if (other.gameObject.CompareTag("WallB"))
+        {
+            rWallB.GetComponent<countCSV>().wallB++;
+        }
+        if (other.gameObject.CompareTag("WallR"))
+        {
+            rWallR.GetComponent<countCSV>().wallR++;
+        }
+        if (other.gameObject.CompareTag("WallL"))
+        {
+            rWallL.GetComponent<countCSV>().wallL++;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ countCSV.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Text;
+using System.IO;
+using UnityEngine.UI;
+
+public class countCSV : MonoBehaviour
+{
+    void Start()
+    {GameObject.Find("CSVStop").GetComponent<Button>().interactable = false;}
+
+    public int wallF = 0;
+    public int wallB = 0;
+    public int wallR = 0;
+    public int wallL = 0;
+
+    public bool saves;
+
+    void Update()
+    {}
+
+    private string DataName;
+    byte filenum_1 = 0;
+    byte filenum_10 = 0;
+    byte filenum_100 = 0;
+
+    public void RecStart()
+    {
+        if (!saves)
+        {
+            saves = true;
+            GameObject.Find("CSVStart").GetComponent<Button>().interactable = false;
+            GameObject.Find("CSVStop").GetComponent<Button>().interactable = true;
+
+            DataName = "000衝突回数.csv";
+            while (true)
+            {
+                if (File.Exists(Application.persistentDataPath + "/" + DataName)) //If it is a stand-alone HMD, the path may not pass, so use the path of the persistent data directory.
+                {
+                    filenum_1++;
+                    if (filenum_1 > 9)
+                    {
+                        filenum_10++;
+                        filenum_1 = 0;
+                        if (filenum_10 > 9)
+                        {
+                            filenum_100++;
+                            filenum_10 = 0;
+                        }
+                    }
+                    DataName = "";
+                    DataName += filenum_100;
+                    DataName += filenum_10;
+                    DataName += filenum_1;
+                    DataName += "衝突回数.csv";
+                }
+                else
+                {
+                    break;
+                }
+            }
+            StreamWriter sw = new StreamWriter(Application.persistentDataPath + "/" + DataName, false); //If it is a stand-alone HMD, the path may not pass, so use the path of the persistent data directory.
+            // ヘッダー出力
+            string[] s1 = { "壁(前)","壁(後)","壁(右)","壁(左)" };
+            string s2 = string.Join(",", s1);
+            sw.WriteLine(s2);
+            sw.Close();
+        }
+        else
+        {
+            StreamWriter sw = new StreamWriter(Application.persistentDataPath + "/" + DataName, true);
+            string[] onepos = { wallF.ToString(), wallB.ToString(), wallR.ToString(), wallL.ToString()};
+            string poslog = string.Join(",", onepos);
+            sw.WriteLine(poslog);
+            sw.Flush();
+            sw.Close();
+
+            saves = false;
+            GameObject.Find("CSVStart").GetComponent<Button>().interactable = true;
+            GameObject.Find("CSVStop").GetComponent<Button>().interactable = false;
+        }
+    }
+}
